dc69b13a3973c1327448d4e5f081acf0b48c4491
Олесуа и Юля <astahova.98@gmail.com>
2018-02-03 09:43:44 +0700

    Определение статического класса TimeUtilClass

---
+++ SimpleUtilityClass/SimpleUtilityClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleUtilityClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Static Classes *****\n");
+
+            TimeUtilClass.PrintDate();
+            TimeUtilClass.PrintTime();
+
+            //TimeUtilClass u = new TimeUtilClass();
+            Console.ReadLine();
+        }
+    }
+}
